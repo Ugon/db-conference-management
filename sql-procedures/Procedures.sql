18a35e7c0f9acec9fd75897d86e8791f834209ae
Piotr Wąchała <piotrwachala27@gmail.com>
2014-12-27 15:03:51 +0100

    Added procedures for person reservation

--- sql-procedures/Procedures.sql
+++ sql-procedures/Procedures.sql
@@ -33,6 +33,37 @@ IF OBJECT_ID('addWorkshopType') IS NOT NULL
 DROP PROC addWorkshopType;
 GO
 
+IF OBJECT_ID('addDayReservation') IS NOT NULL
+DROP PROCEDURE addDayReservation
+GO
+
+IF OBJECT_ID('addWorkshopReservation') IS NOT NULL
+DROP PROCEDURE addWorkshopReservation
+GO
+
+IF OBJECT_ID('addNewReservation') IS NOT NULL
+DROP PROCEDURE addNewReservation
+GO
+
+IF OBJECT_ID('insertPersonIfNotExists') IS NOT NULL
+DROP PROCEDURE insertPersonIfNotExists
+GO
+
+IF OBJECT_ID('addDayReservationDetails') IS NOT NULL
+DROP PROCEDURE addDayReservationDetails
+GO
+
+IF OBJECT_ID('addWorkshopReservationDetails') IS NOT NULL
+DROP PROCEDURE addWorkshopReservationDetails
+GO
+
+IF OBJECT_ID('removeWorkshopReservationDetail') IS NOT NULL
+DROP PROCEDURE removeWorkshopReservationDetail
+GO
+
+IF OBJECT_ID('removeDayReservationDetail') IS NOT NULL
+DROP PROCEDURE removeDayReservationDetail
+GO
 
 CREATE procedure insertIfNotExistsAddress
 	@Country varchar(50),
@@ -41,12 +72,29 @@ CREATE procedure insertIfNotExistsAddress
 	@PostalCode varchar(6)
 AS
 BEGIN
+	SET NOCOUNT ON;
 	if not exists (select * from [Address] as a 
 		where a.Country = @Country and a.City = @City and a.Street = @Street and a.PostalCode = @PostalCode)
 	insert into [Address](Country, City, Street, PostalCode) 
 		values (@Country, @City, @Street, @PostalCode)
 END
 GO
+
+CREATE PROCEDURE insertPersonIfNotExists
+	@FirstName varchar(50),
+	@LastName varchar(50),
+	@Mail varchar(50),
+	@IndexNumber varchar(6) =  null
+AS
+BEGIN
+	SET NOCOUNT ON;
+	if not exists (select * from Person where FirstName = @FirstName and LastName = @LastName
+		and Mail = @Mail)
+	insert into Person(FirstName, LastName, Mail, IndexNumber) 
+		values (@FirstName, @LastName, @Mail, @IndexNumber)
+END
+GO
+
 
 CREATE PROCEDURE addEarlyBirdDiscount
 	@ConferenceName varchar(50),
@@ -79,7 +127,7 @@ CREATE procedure addClientCompany
 	@BankAccount varchar(32)
 AS
 BEGIN
-		set nocount on;
+		SET NOCOUNT ON;
 		declare @addressId int --id adresu ktory dodajemy
 		declare @clientId int --id utworzonego klienta
 			
@@ -120,10 +168,9 @@ BEGIN
 	declare @clientId Int;
 	
 	--tworzenie rekordu Person
-	insert into Person(FirstName,LastName, Mail)
-		values (@FirstName, @LastName, @Mail)
-		
-	set @personId = SCOPE_IDENTITY();
+	exec insertPersonIfNotExists @FirstName, @LastName, @Mail, @IndexNumber
+	set @personId = (select PersonId from Person where FirstName = @FirstName
+		and LastName = @LastName and Mail = @Mail)
 	
 	--tworzenie rekordu Address
 	exec insertIfNotExistsAddress @Country, @City, @Street, @PostalCode
@@ -139,8 +186,8 @@ BEGIN
 	set @clientId = SCOPE_IDENTITY();
 	
 	--tworzenie rekordu PersonClient
-	insert into PersonClient(ClientID,PersonID,IndexNumber) 
-		values (@clientId,@personId,@IndexNumber)
+	insert into PersonClient(ClientID,PersonID,) 
+		values (@clientId,@personId)
 END
 GO
 
@@ -188,12 +235,14 @@ GO
 
 CREATE PROCEDURE addNewPerson
 	@FirstName varchar(50),
-	@LastName varchar(50)
+	@LastName varchar(50),
+	@Mail varchar(50),
+	@IndexNumber varchar(6)
 AS
 BEGIN
 	SET NOCOUNT ON;
-	insert into Person(FirstName, LastName)
-		values(@FirstName, @LastName)
+	
+	exec insertPersonIfNotExists @FirstName, @LastName, @Mail, @IndexNumber
 END
 GO
 
@@ -218,6 +267,8 @@ CREATE PROCEDURE addWorkshopInstance
 	@Location varchar(50)
 AS
 BEGIN
+	SET NOCOUNT ON;
+	
 	declare @dayId int;
 	declare @WorkshopTypeId int;
 	
@@ -229,5 +280,163 @@ BEGIN
 	
 	insert into WorkshopInstance(DayID,Location,WorkshopTypeID,StartTime,EndTime)
 		values (@dayId, @Location, @WorkshopTypeId, @StartTime, @EndTime)
+END
+GO
+
+--=============REZERWACJE=============--
+
+CREATE PROCEDURE addWorkshopReservationDetails
+	@reservationId int,
+	@conferenceName varchar(50),
+	@workshopName varchar(50),
+	@date date,
+	@startTime time,
+	@FirstName varchar(50),
+	@LastName varchar(50),
+	@Mail varchar(50),
+	@IndexNumber varchar(6) =  null
+AS
+BEGIN
+	SET NOCOUNT ON;
+	declare @dayReservationDetailsId int;
+	declare @personId int;
+	declare @dayReservationId int;
+	declare @dayId int;
+	declare @workshopInstanceId int;
+	declare @workshopReservationId int;
+	
+	exec insertPersonIfNotExists @FirstName, @LastName, @Mail, @IndexNumber
+	set @personId = (select PersonId from Person where FirstName = @FirstName
+		and LastName = @LastName and Mail = @Mail)
+		
+	set @dayId = dbo.getConferenceDayId(@conferenceName, @date);
+		
+	set @dayReservationId = (select DayReservationId from DayReservation 
+		where ReservationID = @reservationId and DayID = @dayId)
+		
+	set @dayReservationDetailsId = (select DayReservationDetailsId from DayReservationDetails 
+		where PersonID = @personId and DayReservationID = @dayReservationId)
+		
+	if @dayReservationDetailsId is not null
+	begin
+		set @workshopInstanceId = (select WorkshopInstanceId from WorkshopInstance
+		where DayID = @dayId and StartTime = @startTime 
+		and WorkshopTypeID = (select WorkshopTypeID from WorkshopType where Name = @workshopName))	
+		
+		set @workshopReservationId = (select WorkshopReservationID from WorkshopReservation
+			where DayReservationID = @dayReservationId and WorkshopInstanceID = @workshopInstanceId)
+		
+		insert into WorkshopReservationDetails(DayReservationDetailsID, WorkshopReservationID)
+			values(@dayReservationDetailsId, @workshopReservationId)
+		
+	end
+	else RAISERROR('Given person has no reservation for a conference',0,1)
+END
+GO
+
+CREATE PROCEDURE addDayReservationDetails
+	@reservationId int,
+	@conferenceName varchar(50),
+	@date date,
+	@FirstName varchar(50),
+	@LastName varchar(50),
+	@Mail varchar(50),
+	@IndexNumber varchar(6) = null
+AS
+BEGIN 
+	SET NOCOUNT ON;
+	declare @dayReservationId int;
+	declare @dayId int;
+	declare @personId int;
+	declare @student bit;
+	
+	set @dayId = dbo.getConferenceDayId(@conferenceName, @date);
+	
+	set @dayReservationId = (select DayReservationID from DayReservation
+		where DayID = @dayId and ReservationID = @reservationId)
+		
+	exec insertPersonIfNotExists @FirstName, @LastName, @Mail, @IndexNumber
+	
+	set @personId = (select PersonId from Person where FirstName = @FirstName 
+		and LastName = @LastName and Mail = @Mail)
+	
+	if @IndexNumber is null
+	begin
+		set @student = 0;
+	end
+	else 
+	begin
+		set @student = 1;
+	end
+	
+	insert into DayReservationDetails(DayReservationID, PersonID, Student)
+		values(@dayReservationId, @personId, @student)
+	
+END
+GO 
+
+CREATE PROCEDURE addNewReservation
+	@Login varchar(50),
+	@reservationId int output
+AS
+BEGIN
+	SET NOCOUNT ON;
+	declare @clientId int;
+	
+	set @clientId = (select ClientId from Client where Login = @Login)
+	
+	insert into Reservation(ClientID)
+		values(@clientId)
+		
+	set @reservationId = SCOPE_IDENTITY();
+	
+END
+GO 
+
+CREATE PROCEDURE addDayReservation
+	@reservationId int,
+	@conferenceName varchar(50),
+	@date date,
+	@NumberOfParticipants int,
+	@NumberOfStudentDiscounts int
+AS
+BEGIN
+	SET NOCOUNT ON;
+	
+	declare @dayId int;
+	
+	set @dayId = dbo.getConferenceDayId(@conferenceName, @date)
+		
+	insert into DayReservation(DayID, ReservationID, NumberOfParticipants, NumberOfStudentDiscounts)
+		values (@dayId, @reservationId, @NumberOfParticipants, @NumberOfStudentDiscounts)
+END
+GO
+
+CREATE PROCEDURE addWorkshopReservation
+	@reservationId int,
+	@conferenceName varchar(50),
+	@workshopName varchar(50),
+	@date date,
+	@startTime time,
+	@NumberOfParticipants int,
+	@NumberOfStudentDiscounts int
+AS 
+BEGIN
+	SET NOCOUNT ON;
+	declare @workshopInstanceId int;
+	declare @dayId int;
+	declare @dayReservationId int;
+	
+	set @dayId = dbo.getConferenceDayId(@conferenceName, @date)
+	
+	set @workshopInstanceId = (select WorkshopInstanceId from WorkshopInstance
+		where DayID = @dayId and StartTime = @startTime 
+		and WorkshopTypeID = (select WorkshopTypeID from WorkshopType where Name = @workshopName))
+		
+	set @dayReservationId = (select DayReservationId from DayReservation where DayID = @dayId)
+		
+	insert into WorkshopReservation(WorkshopInstanceID, DayReservationID, 
+		NumberOfParticipants, NumberOfStudentDiscounts)
+		values(@workshopInstanceId, @dayReservationId ,@NumberOfParticipants, @NumberOfStudentDiscounts)
 END
 GO
